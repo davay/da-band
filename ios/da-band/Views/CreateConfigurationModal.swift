4067a1f992613f3b9649d10415b5cf4ad3129b9b
Devin Lim <davay@devinl.im>
2025-08-12 21:24:53 -0700

    wip: configuration modal step 1

--- ios/da-band/Views/CreateConfigurationModal.swift
+++ ios/da-band/Views/CreateConfigurationModal.swift
@@ -3,7 +3,37 @@ import SwiftUI
 struct CreateConfigurationModal: View {
     let onDismiss: () -> Void
 
+    @State private var configurationName: String = ""
+
     var body: some View {
-        Text("ConfigurationDetailsView")
+        Modal(onDismiss: onDismiss) {
+            VStack {
+                Text("Create Configuration")
+                    .font(.title3)
+                    .fontWeight(.bold)
+
+                VStack(alignment: .leading) {
+                    Text("Configuration Name:")
+                        .font(.headline)
+                    TextField("", text: $configurationName)
+                        .textFieldStyle(.roundedBorder)
+                        .padding(.bottom)
+
+                    HStack {
+                        Button("Cancel") {
+                            onDismiss()
+                        }
+                        .buttonStyle(.borderedProminent)
+
+                        Spacer()
+                        Button("Continue") {}
+                            .buttonStyle(.borderedProminent)
+                            .disabled(configurationName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty)
+                    }
+                }
+                .padding(.top)
+            }
+            .padding()
+        }
     }
 }
